d4241f7928c412530ee29697edf359566e15458c
hala-almaimoni <halaalmaimoni@gmail.com>
2019-03-31 23:45:07 +0300

    s

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,47 +2,48 @@ import React, { Component } from "react";
 import apiUrl from "../apiConfig";
 
 class Home extends Component {
-    state = {
-        tools: []
-    }
+  state = {
+    tools: []
+  }
 
-    handleLoginRequest = () => {
-        let url = `${apiUrl}/api/tools`;
-    
-        fetch(url, {
-          mode: "cors",
-          credentials: "include",
-          method: "GET",
-        })
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data)
-            this.setState({ tools: data.tools })})
-     
-          .catch(e => console.log(e));
-      };
+  handleLoginRequest = () => {
+    let url = `${apiUrl}/api/tools`;
 
-      componentDidMount() {
-        this.handleLoginRequest();
-      }
+    fetch(url, {
+      mode: "cors",
+      credentials: "include",
+      method: "GET",
+    })
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data)
+        this.setState({ tools: data.tools })
+      })
 
-    render() {
-        const tools = this.state.tools.map(tools => {
-            return (
-              <div>
-                <h3>Type </h3> <h4>{tools.type} </h4>
-                <h3>Quantity </h3> <h4>{tools.quantity} </h4>
-                <h3>Price </h3> <h4>{tools.price} </h4>
-              /
+      .catch(e => console.log(e));
+  };
+
+  componentDidMount() {
+    this.handleLoginRequest();
+  }
+
+  render() {
+    const tools = this.state.tools.map(tools => {
+      return (
+        <div>
+          <h3>Type </h3> <h4>{tools.type} </h4>
+          <h3>Quantity </h3> <h4>{tools.quantity} </h4>
+          <h3>Price </h3> <h4>{tools.price} </h4>
+          /
               </div>
-            );
-          });
-        return <div>{tools}</div>;
-        
-        
-    }
+      );
+    });
+    return <div>{tools}</div>;
 
-        
+
+  }
+
+
 }
 
 
