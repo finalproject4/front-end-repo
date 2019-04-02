e2c7a8e490911fb720ea019805dabec1434a7f13
Majeed Almaimoni <MajeedM@Majeeds-MacBook-Pro.local>
2019-04-02 10:12:14 +0300

    fexing hres issues

--- src/components/myres.js
+++ src/components/myres.js
@@ -4,7 +4,8 @@ import { getUser } from "../services/AuthService";
 
 class MyRes extends Component {
     state = {
-        reservations: []
+        reservations: [],
+        hreservations: []
     }
 
     handleResRequest = () => {
@@ -24,8 +25,26 @@ class MyRes extends Component {
             .catch(e => console.log(e));
     };
 
+    handleResHRequest = () => {
+        let url = `${apiUrl}/api/user/${getUser().id}/hres`;
+        console.log(getUser().id)
+        fetch(url, {
+            mode: "cors",
+            credentials: "include",
+            method: "GET",
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.setState({ hreservations: data.user.Hreservations })
+            })
+
+            .catch(e => console.log(e));
+    };
+
     componentDidMount() {
         this.handleResRequest();
+        this.handleResHRequest();
     }
 
     render() {
@@ -37,8 +56,20 @@ class MyRes extends Component {
                     <h3>Date </h3> <h4>{reservations.date} </h4>
                 </div>
             );
-        });
-        return <div>{reservations}</div>;
+        }); 
+        const hreservations = this.state.hreservations.map(hreservations => {
+            return (
+                <div>
+                    <h3>User</h3> <h4>{hreservations.user_id} </h4>
+                    <h3>hall </h3> <h4>{hreservations.hall_id} </h4>
+                    <h3>Date </h3> <h4>{hreservations.date} </h4>
+                </div>
+            );
+        }); 
+        return <div>
+        {reservations}
+        {hreservations}
+        </div>;
 
 
     }
