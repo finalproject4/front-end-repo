24862caf910d1c97b017bf99787f99db116d2140
Hajar <hajar@Hajars-MacBook-Air.local>
2019-05-08 16:06:29 +0300

    removing /

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl;
 const apiUrls = {
-  production: "https://cryptic-forest-78079.herokuapp.com/",
+  production: "https://cryptic-forest-78079.herokuapp.com",
   development: "http://localhost:3001"
 };
 
